c1fd461d23a9e548e9f99deb3456e646132035ff
unknown <joa.geldenhuys@gmail.com>
2022-12-19 18:16:57 +0200

    selector services started

--- app/services/selectServices.js
+++ app/services/selectServices.js
@@ -20,7 +20,65 @@ const getPlayers = async ()=>{
     return result;
 }
 
+const getMinimumBet = async ()=>
+{
+    const db = await connector();
+    let result;
+    await db.serialize(async () => {
+        await db.get(
+            'select options from config',[], 
+            (err, row)=>{
+                if(err)
+                {
+                    console.log(err);
+                }else 
+                {
+                    const options = JSON.parse(row.options)
+                    const betValue1 = options.betValue1
+                    const betValue2 = options.betValue2
+                    const betValue3 = options.betValue3
+
+                    const valueArray  = [betValue1, betValue2, betValue3]
+                    result = (valueArray.sort((a, b)=>a - b))[0]
+                }
+            }
+        );
+        
+    })
+    return result
+}
+
+
+const getBets = async () => {
+    const db = await connector();
+    let result;
+    await db.serialize(async () => {
+        await db.get(
+            'select options from config',[], 
+            (err, row)=>{
+                if(err)
+                {
+                    console.log(err);
+                }else 
+                {
+                    const options = JSON.parse(row.options)
+                    const betValue1 = options.betValue1
+                    const betValue2 = options.betValue2
+                    const betValue3 = options.betValue3
+
+                    const valueArray  = [betValue1, betValue2, betValue3]
+                    result = valueArray
+                }
+            }
+        );
+        
+    })
+    return result
+}
+
 
 export default {
-    getPlayers
+    getPlayers, 
+    getMinimumBet,
+    getBets
 }
--- build/handlerFunctions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var player = function player(bet, betAmount, balance) {};
-var house = function house(balance) {};
-var outCome = function outCome() {};
-var coin = function coin() {};
-var csvCompiler = function csvCompiler(data) {};
