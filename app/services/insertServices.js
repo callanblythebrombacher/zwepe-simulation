3a46f5f704258eb2e6657d0835629cef324b3fa3
unknown <joa.geldenhuys@gmail.com>
2022-12-20 19:22:14 +0200

    Store Bet Results done

--- app/services/insertServices.js
+++ app/services/insertServices.js
@@ -1,20 +1,47 @@
-import connector from "../database/connector";
+import connector from '../database/connector';
 
 const storeBets = async (bets) => {
-    const {playerID, betValue, headsOrTails} = bets
+    const { playerID, betValue, headsOrTails } = bets;
 
     const db = await connector();
     await db.serialize(async () => {
-        await db.run('insert into ledger (player_id, bet_total, bet_item) VALUES (?,?,?)', [playerID, betValue, headsOrTails], (err) => {
-            if (err) {
-                console.log(err);
-            }else{
-                return true
+        await db.run(
+            'insert into ledger (player_id, bet_total, bet_item) VALUES (?,?,?)',
+            [playerID, betValue, headsOrTails],
+            (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    return true;
+                }
             }
-        });
+        );
+    });
+};
+
+const storeBetResults = async (betResults) => 
+{
+    const { flipResult, totalLosings, totalWinnings, houseTotal } = betResults;
+
+    const db = await connector();
+
+    await db.serialize(async () => 
+    {
+        await db.run(
+            'insert into bets(heads_or_tails,total_losings,total_winnings,house_winnings) VALUES (?,?,?,?)',
+            [flipResult, totalLosings, totalWinnings, houseTotal],
+            (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    return true;
+                }
+            }
+        );
     });
 };
 
 export default {
-    storeBets
-}
+    storeBets,
+    storeBetResults,
+};
